8863e4dd9a5ddb4e54387936afd33a798ec754f5
athulmenondev <athulmenondev@gmail.com>
2025-01-13 12:57:01 +0530

    fixed issue

--- src/pages/Home/Subcomponents/Section.jsx
+++ src/pages/Home/Subcomponents/Section.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import "./Section.css";
-import careright from "./../../../assets/caret-right-solid.svg"
-
 
 const Section = () => {
-  return (
-    <div className="section-container">
-      <div className="arrow"><img src={careright} alt="careright"/></div>
-      <div className="content">
-        <span className="icon">&gt;</span>
-        <span>Published Newsletter for the year 2019-20</span>
-      </div>
-    </div>
-  );
+    return (
+        <div className="section-container">
+            <div className="arrow"></div>
+            <div className="content">
+                <span className="icon"></span>
+                <span>Published Newsletter for the year 2019-20</span>
+            </div>
+        </div>
+    );
 };
 
 export default Section;
